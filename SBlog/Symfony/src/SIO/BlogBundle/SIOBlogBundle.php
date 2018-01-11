163127309d26b6b3ca2ab3896cd581199922b493
SIO-SM-49\lecomtek <lecomtekny@gmail.com>
2018-01-11 10:43:14 +0100

    Suppression des fichiers inutiles

--- SBlog/Symfony/src/SIO/BlogBundle/SIOBlogBundle.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace SIO\BlogBundle;
-
-use Symfony\Component\HttpKernel\Bundle\Bundle;
-
-class SIOBlogBundle extends Bundle
-{
-}
--- SBlog/Symfony/src/Transnat/BlogBundle/TransnatBlogBundle.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Transnat\BlogBundle;
-
-use Symfony\Component\HttpKernel\Bundle\Bundle;
-
-class TransnatBlogBundle extends Bundle
-{
-}
