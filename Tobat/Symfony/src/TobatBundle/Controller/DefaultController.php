67a1bee121d71a85dbcecc6bf49d4d34faff8c20
SIO-SM-49\hemissia <hemissia@saintemarie-cholet.eu>
2018-01-18 11:32:23 +0100

    Création des routes, du contrôleur et ajout des vues

--- Tobat/Symfony/src/TobatBundle/Controller/DefaultController.php
+++ Tobat/Symfony/src/TobatBundle/Controller/DefaultController.php
@@ -10,4 +10,29 @@ class DefaultController extends Controller
     {
         return $this->render('TobatBundle:Default:index.html.twig');
     }
+
+    public function enqueteAction()
+    {
+    	return $this->render('TobatBundle:Default:enquete.html.twig');
+    }
+
+    public function repartitionVisiteurAction()
+    {
+    	return $this->render('TobatBundle:Default:repartition.html.twig');
+    }
+
+    public function visiteursAction()
+    {
+    	return $this->render('TobatBundle:Default:visiteurs.html.twig');
+    }
+
+    public function budgetAction()
+    {
+    	return $this->render('TobatBundle:Default:budget.html.twig');
+    }
+
+    public function classementAction()
+    {
+    	return $this->render('TobatBundle:Default:classement.html.twig');
+    }
 }
