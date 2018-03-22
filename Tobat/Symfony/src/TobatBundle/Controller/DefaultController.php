788a533a588eb7aabede02dbc6125ea3d35417e0
SIO-SM-49\lecomtek <lecomtekny@gmail.com>
2018-03-22 11:11:31 +0100

    Méthodes récupération des statistiques

--- Tobat/Symfony/src/TobatBundle/Controller/DefaultController.php
+++ Tobat/Symfony/src/TobatBundle/Controller/DefaultController.php
@@ -30,6 +30,108 @@ class DefaultController extends Controller
         return new Response($serializer->serialize($client, 'json'));
     }
 
+    public function getNbVisiteursCategoriesSociales()
+    {
+        $nbVisiteursCategories = array();
+
+        $entityManager = $this->getDoctrine()->getManager();
+
+        $categoriesSociales = $entityManager->getRepository('TobatBundle:CategorieSociale')->findAll();
+
+        foreach ($categoriesSociales as $categorieSociale) 
+        {
+            $query = $entityManager->createQuery(
+            'SELECT count(e)
+            FROM TobatBundle:Enquete e, TobatBundle:CategorieSociale c
+            WHERE e.budget=c.id AND c.nomCategorie=:categorie'
+            
+            )->setParameter('categorie', $categorieSociale->getNomCategorie());
+
+            $resultat = $query->getResult();
+
+            $nbVisiteursCategorie = array('nomCategorie'=>$categorieSociale->getNomCategorie(), 'nbVisiteurs' =>$resultat[0][1]);
+
+            array_push($nbVisiteursCategories, $nbVisiteursCategorie);
+        }
+
+        return $nbVisiteursCategories;
+    }
+
+    public function getNbVisiteursTranchesAge()
+    {
+        $nbVisiteursTranchesAge = array();
+
+        $entityManager = $this->getDoctrine()->getManager();
+
+        $query = $entityManager->createQuery(
+            'SELECT DISTINCT(e.trancheAge)
+            FROM TobatBundle:Enquete e'
+        );
+
+        $tranchesAge = $query->getResult();
+
+        foreach ($tranchesAge as $trancheAge) 
+        {
+            $query = $entityManager->createQuery(
+            'SELECT count(e)
+            FROM TobatBundle:Enquete e
+            WHERE e.trancheAge=:trancheAge'
+            
+            )->setParameter('trancheAge', $trancheAge);
+
+            $resultat = $query->getResult();
+
+            $nbVisiteursTrancheAge = array('trancheAge'=>$trancheAge, 'nbVisiteurs' =>$resultat[0][1]);
+
+            array_push($nbVisiteursTranchesAge, $nbVisiteursTrancheAge);
+        }
+
+        return $nbVisiteursTranchesAge;
+    }
+
+    public function getNbVisiteursMotivations()
+    {
+        $nbVisiteursMotivations = array();
+
+        $entityManager = $this->getDoctrine()->getManager();
+
+        $query = $entityManager->createQuery(
+            'SELECT DISTINCT(e.motivation)
+            FROM TobatBundle:Enquete e'
+        );
+
+        $motivations = $query->getResult();
+
+        foreach ($motivations as $motivation) 
+        {
+            $query = $entityManager->createQuery(
+            'SELECT count(e)
+            FROM TobatBundle:Enquete e
+            WHERE e.motivation=:motivation'
+            
+            )->setParameter('motivation', $motivation);
+
+            $resultat = $query->getResult();
+
+            $nbVisiteursMotivation = array('motivation'=>$motivation, 'nbVisiteurs' =>$resultat[0][1]);
+
+            array_push($nbVisiteursMotivations, $nbVisiteursMotivation);
+        }
+
+        return $nbVisiteursMotivations;
+    }
+
+    public function nbVisiteursCritereAction()
+    {
+        $nbVisiteursTranchesAge = $this->getNbVisiteursTranchesAge();
+
+        $nbVisiteursCategories = $this->getNbVisiteursCategoriesSociales();
+
+        $nbVisiteursMotivations = $this->getNbVisiteursMotivations();
+
+        return $this->render('TobatBundle:Default:nbVisiteursCritere.html.twig', array('nbVisiteursCategories' => $nbVisiteursCategories, 'nbVisiteursTranchesAge' => $nbVisiteursTranchesAge, 'nbVisiteursMotivations' => $nbVisiteursMotivations));
+    }
+
     public function enqueteAction()
     {
     	return $this->render('TobatBundle:Default:enquete.html.twig');
