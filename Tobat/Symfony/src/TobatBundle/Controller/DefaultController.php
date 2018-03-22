13a70214a87711829572abdae295be0f163c51f1
SIO-SM-49\lecomtek <lecomtekny@gmail.com>
2018-03-22 08:13:03 +0100

    Tests Connexion BDD Android + retouche gitignore

--- Tobat/Symfony/src/TobatBundle/Controller/DefaultController.php
+++ Tobat/Symfony/src/TobatBundle/Controller/DefaultController.php
@@ -4,12 +4,30 @@ namespace TobatBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use TobatBundle\Entity\Connexion;
+use Symfony\Component\Serializer\Serializer;
+use TobatBundle\Entity\Bateau;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Serializer\Normalizer\GetSetMethodNormalizer;
+use Symfony\Component\Serializer\Encoder\XmlEncoder;
+use Symfony\Component\Serializer\Encoder\JsonEncoder;
 
 class DefaultController extends Controller
 {
     public function indexAction()
     {
         return $this->render('TobatBundle:Default:index.html.twig');
+    }
+
+    public function testAction()
+    {
+        // Instance de Serializer
+        $serializer = new Serializer(array(new getSetMethodNormalizer()),array(new XMLEncoder(),new JsonEncoder()));
+        // Instance du manager de Doctrine
+        $manager = $this->getDoctrine()->getManager();
+        // Récupération du client
+        $client = $manager->getRepository(Bateau::class)->findAll();
+
+        return new Response($serializer->serialize($client, 'json'));
     }
 
     public function enqueteAction()
